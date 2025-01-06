e879d26519ee49e2354d1c0f9867c2e08d2d69c2
Filip <dudziaky07@gmail.com>
2025-01-06 16:57:09 +0100

    Poprawiono ograniczenia roku produkcji pojazdu (do bieżącego roku)

--- MotoLinker/Models/Announcement.cs
+++ MotoLinker/Models/Announcement.cs
@@ -20,8 +20,20 @@ namespace MotoLinker.Models
         [Required]
         public string Model { get; set; }
         [Required]
-        [Range(1886, int.MaxValue, ErrorMessage = "Rok produkcji musi byæ pomiêdzy 1886 a {1}.")]
-        public int ProductionYear { get; set; }
+        [Range(1886, int.MaxValue, ErrorMessage = "Rok produkcji musi byæ pomiêdzy 1886 a bie¿¹cym rokiem.")]
+        public int ProductionYear
+        {
+            get => _productionYear;
+            set
+            {
+                if (value > DateTime.Now.Year)
+                {
+                    throw new ValidationException($"Rok produkcji nie mo¿e byæ wiêkszy ni¿ {DateTime.Now.Year}.");
+                }
+                _productionYear = value;
+            }
+        }
+        private int _productionYear;
         public DateTime DateAdded { get; set; } = DateTime.Now; // Data dodania og³oszenia
         public int UserId { get; set; } // Powi¹zanie og³oszenia z u¿ytkownikiem
     }
