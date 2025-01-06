12df535e4a2ccc58acea799b6cabf8dcb1cf89eb
Filip <dudziaky07@gmail.com>
2025-01-06 17:07:07 +0100

    Poprawa komunikatów roku produkcji

--- MotoLinker/Models/Announcement.cs
+++ MotoLinker/Models/Announcement.cs
@@ -1,40 +1,50 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace MotoLinker.Models
 {
-    public class Announcement
+    public class Announcement : IValidatableObject
     {
         public int AnnouncementId { get; set; } // ID og³oszenia
+
         [Required]
         [StringLength(100)]
         public string Title { get; set; }
+
         [Required]
         public string Description { get; set; }
+
         [Required]
         [DataType(DataType.Currency)]
         public int Price { get; set; }
+
         [Required]
         public string Location { get; set; }
+
         [Required]
         public string Brand { get; set; }
+
         [Required]
         public string Model { get; set; }
+
         [Required]
-        [Range(1886, int.MaxValue, ErrorMessage = "Rok produkcji musi byæ pomiêdzy 1886 a bie¿¹cym rokiem.")]
-        public int ProductionYear
+        [Range(1886, int.MaxValue, ErrorMessage = "Rok produkcji nie mo¿e byæ mniejszy ni¿ 1886.")]
+        public int ProductionYear { get; set; }
+
+        public DateTime DateAdded { get; set; } = DateTime.Now; // Data dodania og³oszenia
+
+        public int UserId { get; set; } // Powi¹zanie og³oszenia z u¿ytkownikiem
+
+        // Walidacja dynamiczna
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            get => _productionYear;
-            set
+            if (ProductionYear > DateTime.Now.Year)
             {
-                if (value > DateTime.Now.Year)
-                {
-                    throw new ValidationException($"Rok produkcji nie mo¿e byæ wiêkszy ni¿ {DateTime.Now.Year}.");
-                }
-                _productionYear = value;
+                yield return new ValidationResult(
+                    $"Rok produkcji nie mo¿e byæ wiêkszy ni¿ {DateTime.Now.Year}.",
+                    new[] { nameof(ProductionYear) });
             }
         }
-        private int _productionYear;
-        public DateTime DateAdded { get; set; } = DateTime.Now; // Data dodania og³oszenia
-        public int UserId { get; set; } // Powi¹zanie og³oszenia z u¿ytkownikiem
     }
 }
