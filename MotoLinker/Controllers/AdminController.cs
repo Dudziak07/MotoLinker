ec8c7d92db017a7842023b5afecaefcaffe0a746
Filip <dudziaky07@gmail.com>
2025-01-10 14:13:03 +0100

    Poprawka przekierowań w ManageUsers

--- MotoLinker/Controllers/AdminController.cs
+++ MotoLinker/Controllers/AdminController.cs
@@ -62,7 +62,7 @@ namespace MotoLinker.Controllers
             }
 
             TempData["Message"] = "Dane użytkownika zostały zaktualizowane.";
-            return RedirectToAction("Index");
+            return RedirectToAction("ManageUsers");
         }
 
         public IActionResult DeleteUser(int id)
@@ -89,7 +89,7 @@ namespace MotoLinker.Controllers
 
             _users.Remove(user);
             TempData["Message"] = "Użytkownik został usunięty.";
-            return RedirectToAction("Index");
+            return RedirectToAction("ManageUsers");
         }
 
         public IActionResult MakeAdmin(int id)
@@ -103,7 +103,7 @@ namespace MotoLinker.Controllers
 
             user.IsAdmin = true;
             TempData["Message"] = $"Użytkownik {user.Username} został administratorem.";
-            return RedirectToAction("Index");
+            return RedirectToAction("ManageUsers");
         }
 
         [HttpPost]
@@ -123,7 +123,7 @@ namespace MotoLinker.Controllers
                 ? "Użytkownik został administratorem."
                 : "Uprawnienia administratora zostały odebrane.";
 
-            return RedirectToAction("Index");
+            return RedirectToAction("ManageUsers");
         }
 
         public IActionResult ManageCategories()
